4018e6ffc5b47f8c129067bf59a273365f412376
cjlxka <71208790+cjlxka@users.noreply.github.com>
2023-07-10 20:48:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,14 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  // appid: "wx71941f6106ae1072",
+  appid: "wx68ad7b57fac56f2b",
+  //appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
+  appsecret: "b0960ddb068c91129097dfcf24f6157a",
+  // template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  template_id: "AnQuJgdFAFRzko7t1aNjr1_TFGJXvhFc7F9x2bfdl7g",
+  // touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "oiMYV6WrBR2d9WG54GRTi_3P52v8",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "衢州",
+  first_date: "2018-8-2",
 };
